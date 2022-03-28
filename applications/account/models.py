417b0e0c8637f7f50894da4992a2b85dfb2711f3
g1ftmoon <orozakunov2222@gmail.com>
2022-03-28 09:50:33 +0600

    swagger documents

--- applications/account/models.py
+++ applications/account/models.py
@@ -53,6 +53,7 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=50, blank=True, null=True)
     age = models.IntegerField(blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
+    number = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.user.email
--- market_place/urls.py
+++ market_place/urls.py
@@ -2,7 +2,25 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+
 from market_place import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Market_place",
+      default_version='v1',
+      description="This is simple online store project",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +29,8 @@ urlpatterns = [
     path('category/', include('applications.category.urls')),
     path('order/', include('applications.order.urls')),
     path('review/', include('applications.review.urls')),
+    path('swagger(.json|.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
